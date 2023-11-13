8ba11db48c15eef28250c31786e95650ab51c8cd
ndaeila <ndaeila@uw.edu>
2023-11-13 10:09:42 -0800

    Setting up NextJS app :^/

---
+++ com-daeila-www/src/pages/about.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Layout from '../components/Layout';
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>IDK I&apos;M A STUDENT OR SOMETHING</h1>
+      <h2>Please hire me</h2>
+    </Layout>
+  );
+}
+
+export default Home();
---
+++ com-daeila-www/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Layout from '../components/Layout';
+
+// const inter = Inter({ subsets: ['latin'] });
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Nathan Daeila</h1>
+      {/** Yo */}
+    </Layout>
+  );
+};
+
+export default Home;
